a6886c440612eea57a1f03fdbc9b9cf6b15b1b32
Rogan <rogan@aimmo.co.kr>
2023-01-23 17:03:04 +0900

    chore: 프로젝트 이름 변경 (omoji)

--- android/app/src/main/java/com/rntest/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/rntest/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -41,7 +41,7 @@ public class MainApplicationTurboModuleManagerDelegate
     if (!sIsSoLibraryLoaded) {
       // If you change the name of your application .so file in the Android.mk file,
       // make sure you update the name here as well.
-      SoLoader.loadLibrary("rntest_appmodules");
+      SoLoader.loadLibrary("omoji_appmodules");
       sIsSoLibraryLoaded = true;
     }
   }
