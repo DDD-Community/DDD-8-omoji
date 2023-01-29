e34de98e5ceadcdd1180e7c45e0cd9fdf3bb3318
sa02045 <sa02045@naver.com>
2023-01-29 12:18:18 +0900

    feat: post API

---
+++ src/api/posts.js
@@ -0,0 +1,25 @@
+import axios from './core';
+
+export const requestPostPosts = async data => {
+  return axios.post('/posts', data);
+};
+
+export const requestGetPost = async id => {
+  return axios.get(`/posts/${id}`);
+};
+
+export const requestDeletePost = async id => {
+  return axios.delete(`/posts/${id}`);
+};
+
+export const requestPutPost = async (id, data) => {
+  return axios.put(`/posts/${id}`, data);
+};
+
+export const requestGetMainPosts = async (start, limit) => {
+  return axios.get(`/posts?start=${start}&limit=${limit}`);
+};
+
+export const requestGetMyPosts = async (start, limit) => {
+  return axios.get(`/posts/profile?start=${start}&limit=${limit}`);
+};
