29cbdbee25b24a46bdd90c8f87cb90257909594f
sa02045 <sa02045@naver.com>
2023-01-29 15:01:44 +0900

    feat: Tag 컴포넌트

---
+++ src/component/Tag.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export function Tag({title}) {
+  return (
+    <View style={styles.tag}>
+      <Text>{title}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  tag: {
+    backgroundColor: '#fff',
+    height: 24,
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    borderRadius: 8,
+    padding: 3,
+    paddingLeft: 8,
+    paddingRight: 8,
+  },
+});
